a93cf2665a50577d822cab9a98c8f241739b7b29
brethitman <yosueanverson12@gmail.com>
2025-10-02 22:25:32 -0400

    chore: usar .env para consumir backend

--- app/page.tsx
+++ app/page.tsx
@@ -1,13 +1,17 @@
 // app/page.tsx
 export default async function Home() {
-  // Detecta la URL del backend según el entorno
-  const apiUrl =
-    process.env.NODE_ENV === "production"
-      ? "https://backend-express.vercel.app/api/hello" // URL de Vercel cuando despliegues
-      : "http://localhost:3001/api/hello"; // Local
+  const apiUrl = process.env.NEXT_PUBLIC_API_URL; // viene del .env
 
-  const res = await fetch(apiUrl, { cache: "no-store" });
-  const data = await res.json();
+  let data = { message: "No se pudo conectar al backend" };
+
+  try {
+    const res = await fetch(`${apiUrl}/api/hello`, { cache: "no-store" });
+    if (res.ok) {
+      data = await res.json();
+    }
+  } catch (error) {
+    console.error("Error al consumir API:", error);
+  }
 
   return (
     <main className="flex flex-col items-center justify-center min-h-screen bg-gray-100">
@@ -15,9 +19,7 @@ export default async function Home() {
         Frontend en Next.js 🚀
       </h1>
       <p className="text-lg text-gray-800">Respuesta desde el backend:</p>
-      <div className="mt-4 px-4 py-2 bg-white rounded-lg shadow">
-        <pre className="text-green-600 font-mono">{data.message}</pre>
-      </div>
+      <pre className="text-green-600">{data.message}</pre>
     </main>
   );
 }
